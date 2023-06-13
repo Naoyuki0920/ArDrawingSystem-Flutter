e72460e7bd9106243c28fb4de65317070e73e91b
Naoyuki0920 <naoyuki.effort110920@gmail.com>
2023-06-13 16:53:10 +0900

    AR描画ページ追加

--- lib/drawing_ar.dart
+++ lib/drawing_ar.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
 
 class DrawingAR extends StatelessWidget {
   const DrawingAR({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("AR"));
+    return ModelViewer(
+      src: 'https://modelviewer.dev/shared-assets/models/NeilArmstrong.glb',
+      ar: true,
+      autoRotate: true,
+      cameraControls: true,
+    );
   }
 }
